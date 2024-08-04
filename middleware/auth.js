0011e0a837b5d20f8aff4cc4ec388fb41fef9df5
Siva-Chengu <sivakumara.chengubraydoo@umail.uom.ac.mu>
2024-08-04 19:25:01 +0400

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,23 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
+    // Get token from header
     const token = req.header('x-auth-token');
+
+    // Check if no token
     if (!token) {
         return res.status(401).json({ msg: 'No token, authorization denied' });
     }
+
     try {
+        // Verify token
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded.userId;
+        
+        // Add user from payload
+        req.user = decoded;
         next();
     } catch (err) {
+        console.error('Token verification failed:', err.message);
         res.status(401).json({ msg: 'Token is not valid' });
     }
 };
