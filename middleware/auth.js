79610385a3b5558bb77bbec2926be6a1f71c7685
siva-chengu <sivakumarachengubraydoo@gmail.com>
2024-08-05 09:55:41 +0400

    added console.log

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = function (req, res, next) {
     // Get token from header
     const token = req.header('x-auth-token');
+    console.log(token)
 
     // Check if no token
     if (!token) {
