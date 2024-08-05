909e73a3cbc4f36ca63163275e79af7528c513d3
siva-chengu <sivakumarachengubraydoo@gmail.com>
2024-08-05 09:25:37 +0400

    added console.log

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(express.json());
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/reminders', reminderRoutes);
+console.log|("Hello")
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
